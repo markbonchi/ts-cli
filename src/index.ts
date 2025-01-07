90126ce5d19997f87c61daeb04803f15dda7bedf
markbonchi <markbonchi@gmail.com>
2025-01-07 12:43:57 +0100

    Preparing interactive interface tp parse necessary commands

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,42 @@
 #!/usr/bin/env node
 import fs from "fs"
 import { Command } from "commander"
+import readline from "readline/promises"
+import { stdin as input, stdout as output, stderr } from "process"
+import inquirer from "inquirer"
 
-const program = new Command()
+// const program = new Command()
 
-program
-    .action(() => { console.log("Hello!") })
+// inquirer.prompt([])
+
+// program
+//     .argument("<string>", "string to log")
+//     .action((message: string) => { console.log(`Hello ${message}!`) })
 
 
-program.parse(process.argv)
+// program.parse(process.argv)
+
+
+class main {
+    rl: readline.Interface
+    constructor() {
+        const rl = readline.createInterface({ input, output });
+        this.rl = rl;
+        this.rl.setPrompt("(cmd)> ");
+        this.rl.prompt();
+    }
+
+    mainloop = async () => {
+        for await (const line of this.rl) {
+            if (line === "quit") {
+                console.log("exit");
+                this.rl.close();
+            } else {
+                this.rl.prompt();
+                this.mainloop();
+            }
+        }
+    };
+}
+
+new main().mainloop();
