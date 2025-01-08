d3f59b37c7b68e81aa65374c5dcf457063356ac3
markbonchi <markbonchi@gmail.com>
2025-01-08 12:06:05 +0100

    reafactored the cmd class

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,7 @@
 import readline from "readline/promises"
 import { stdin as input, stdout as output, stderr } from "process"
 
+
 export class cmd {
     rl: readline.Interface
     constructor() {
@@ -12,14 +13,22 @@ export class cmd {
     mainloop = async () => {
         this.rl.prompt();
         for await (const line of this.rl) {
-            if (line === "quit") {
-                console.log("exit");
-                this.rl.close();
-            } else {
-                this.mainloop();
-            }
+            this.parceCommand(line)
         }
     };
+
+    protected parceCommand = (line: string) => {
+        switch (line) {
+            case "quit":
+                this.rl.close();
+                console.log("You quit");
+                break;
+            case "clear":
+                console.log("clear")
+            default:
+                this.mainloop()
+        }
+    }
 }
 
 export default { cmd }
